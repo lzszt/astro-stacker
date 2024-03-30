da6975bc15ea2af80589e5fcbd9168c8bcc7e74d
Felix Leitz <felix.leitz92@gmail.com>
2024-03-30 09:47:02 +0100

    Adds alignment computation.

--- src-lib/MyLib.hs
+++ src-lib/MyLib.hs
@@ -9,20 +9,17 @@ module MyLib where
 import Codec.Picture qualified as P
 import Codec.Picture.Types qualified as P
 import Control.Concurrent.Async qualified as Async
-import Control.Monad
 import Data.Foldable
-import Data.List (foldl1')
+import Data.Function
+import Data.List
 import Data.Maybe
 import Data.Time (diffUTCTime)
 import Data.Vector.Storable qualified as VS
 import Data.Vector.Storable qualified as Vector
-import Data.Word
 import Debug.Trace
 import Effects.Cache qualified as Cache
 import Effects.Logging qualified as Log
 import Effects.Time qualified as Time
-import Graphics.Image qualified as Graphics
-import Graphics.Image.Interface qualified as Graphics
 import Polysemy
 import Polysemy.Reader qualified as Reader
 import System.Directory
@@ -33,7 +30,7 @@ data StarLocation = StarLocation
   { starLocationX :: Int,
     starLocationY :: Int
   }
-  deriving (Show)
+  deriving (Show, Eq, Ord)
 
 data Alignment = Alignment
   { offsetX :: Int,
@@ -53,8 +50,6 @@ data StarsLocated
 data Aligned
 
 type Tiff = P.Image P.PixelRGB16
-
--- Graphics.Image Graphics.VS Graphics.RGB Double
 
 data Image a where
   Raw :: String -> Tiff -> Image Raw
@@ -310,6 +305,68 @@ data OuterCorners = OuterCorners
   }
   deriving (Show)
 
+stars540 :: [StarLocation]
+stars540 = map (uncurry StarLocation) [(644, 1393), (2764, 4991), (3389, 454)]
+
+alignment540 :: Alignment
+alignment540 = Alignment 0 0 0
+
+stars541 :: [StarLocation]
+stars541 = map (uncurry StarLocation) [(645, 1385), (2768, 4987), (3392, 447)]
+
+alignment541 :: Alignment
+alignment541 = computeAlignment stars540 stars541
+
+computeAlignment :: [StarLocation] -> [StarLocation] -> Alignment
+computeAlignment sts1 sts2 = minimumBy (compare `on` alignmentError) [Alignment offX offY rot | rot <- [-1, -0.9 .. 1], offX <- [-20, -19 .. 20], offY <- [-20, -19 .. 20]]
+  where
+    alignmentError :: Alignment -> Int
+    alignmentError alg =
+      sum $
+        zipWith starDistance (sort sts1) $
+          map (\(StarLocation x y) -> uncurry StarLocation $ applyAlignment alg (x, y)) $
+            sort sts2
+
+testImgs :: IO [(Tiff, Alignment)]
+testImgs = do
+  img540 <- drawStarLocations stars540 . getTiff <$> loadTiff "./resources/lights/DSC00540.tiff"
+  img541 <- drawStarLocations stars541 . getTiff <$> loadTiff "./resources/lights/DSC00541.tiff"
+  pure
+    [ (img540, alignment540),
+      (img541, traceShowId alignment541)
+    ]
+
+-- stars1 = [StarLocation 10 10, StarLocation 10 30, StarLocation 20 80]
+
+-- stars2 = [StarLocation 15 15, StarLocation 15 35, StarLocation 25 85]
+
+-- testImgs =
+--   [ ( P.generateImage (\x y -> if StarLocation x y `elem` stars1 then P.PixelRGB16 maxBound maxBound maxBound else PixelRGB16 0 0 0) 40 100,
+--       Alignment 0 0 0
+--     ),
+--     ( P.generateImage (\x y -> if StarLocation x y `elem` stars2 then P.PixelRGB16 maxBound maxBound maxBound else PixelRGB16 0 0 0) 40 100,
+--       Alignment (-5) (-5) 0
+--     )
+--   ]
+
+test = testImgs >>= P.writeTiff "./resources/tmp/stack_correct.tiff" . stackImages
+
+starDistance :: StarLocation -> StarLocation -> Int
+starDistance (StarLocation x1 y1) (StarLocation x2 y2) =
+  (x1 - x2) ^ 2 + (y1 - y2) ^ 2
+
+applyAlignment :: Alignment -> (Int, Int) -> (Int, Int)
+applyAlignment Alignment {..} = translate . rotate rotation
+  where
+    translate :: (Int, Int) -> (Int, Int)
+    translate (x, y) = (x + offsetX, y + offsetY)
+
+rotate :: Double -> (Int, Int) -> (Int, Int)
+rotate rotation (x, y) =
+  let x' = fromIntegral x
+      y' = fromIntegral y
+   in (round $ x' * cos rotation - y' * sin rotation, round $ y' * cos rotation + x' * sin rotation)
+
 stackImages :: [(Tiff, Alignment)] -> P.Image P.PixelRGBA16
 stackImages [] = error "cannot stack empty set of images"
 stackImages images@((P.Image w h _, Alignment offX offY rot) : sources) =
@@ -319,7 +376,7 @@ stackImages images@((P.Image w h _, Alignment offX offY rot) : sources) =
 
     (outputWidth, outputHeight, upperLeft) =
       outerCornersToDimensions
-        $ foldl
+        $ foldl'
           (\cor (P.Image {..}, alg) -> updateCorners cor imageHeight imageWidth alg)
           (OuterCorners (0, 0) (w, h))
         $ map (fmap ajustAlignment) sources
@@ -339,17 +396,11 @@ stackImages images@((P.Image w h _, Alignment offX offY rot) : sources) =
         (lrX - ulX, lrY - ulY, ul)
 
     updateCorners :: OuterCorners -> Int -> Int -> Alignment -> OuterCorners
-    updateCorners cor height width Alignment {..} =
+    updateCorners cor height width alg =
       let imageCorners = [(0, 0), (0, height), (width, 0), (width, height)]
-          translate (x, y) = (x + offsetX, y + offsetY)
-          alignedImageCorners = map (translate . rotate rotation) imageCorners
-       in foldl' applyCorner cor alignedImageCorners
 
-    rotate :: Double -> (Int, Int) -> (Int, Int)
-    rotate rotation (x, y) =
-      let x' = fromIntegral x
-          y' = fromIntegral y
-       in (round $ x' * cos rotation - y' * sin rotation, round $ y' * cos rotation + x' * sin rotation)
+          alignedImageCorners = map (applyAlignment alg) imageCorners
+       in foldl' applyCorner cor alignedImageCorners
 
     applyCorner :: OuterCorners -> (Int, Int) -> OuterCorners
     applyCorner
@@ -365,9 +416,7 @@ stackImages images@((P.Image w h _, Alignment offX offY rot) : sources) =
 
     lookupPixel :: (Int, Int) -> Int -> Int -> (Tiff, Alignment) -> Maybe P.PixelRGB16
     lookupPixel (ulX, ulY) outX outY (img@P.Image {..}, Alignment offX offY rot) =
-      let (x', y') = rotate (negate rot) (outX + ulX, outY + ulY)
-          x = x' + offX
-          y = y' + offY
+      let (x, y) = rotate (negate rot) (outX + ulX - offX, outY + ulY - offY)
        in if x >= 0
             && x < imageWidth
             && y >= 0
@@ -438,57 +487,59 @@ newtype StarLocationKey = StartLocationKey {getLocationKey :: String}
   deriving (Show)
 
 run :: Log.Severity -> [FilePath] -> [FilePath] -> [FilePath] -> FilePath -> IO ()
-run logSeverity darks biass lights workingDir = runM $
-  Reader.runReader (WorkingDir workingDir) $
-    Time.runTime $
-      Log.runLoggingWithTime (logConfig logSeverity) $
-        runStarsLocatedCache $
-          runMasterCache $ do
-            Log.logInfo $ "Darks: " <> show (length darks)
-            Log.logInfo $ "Biass: " <> show (length biass)
-            Log.logInfo $ "Lights: " <> show (length lights)
-            Log.logInfo $ "Working directory:  " <> workingDir
-            (masterDark, masterBias) <-
-              embed $
-                runM
-                  ( Reader.runReader (WorkingDir workingDir) $
-                      Time.runTime $
-                        Log.runLoggingWithTime (logConfig logSeverity) $
-                          withTiming "MasterDark" $
-                            runMasterCache $
-                              calculateMaster "master_dark.tiff" darks
-                  )
-                  `Async.concurrently` runM
-                    ( Reader.runReader (WorkingDir workingDir) $
-                        Time.runTime $
-                          Log.runLoggingWithTime (logConfig logSeverity) $
-                            withTiming "MasterBias" $
-                              runMasterCache $
-                                calculateMaster "master_bias.tiff" biass
-                    )
+run logSeverity darks biass lights workingDir = test
 
-            cleanImages <-
-              embed $
-                Async.mapConcurrently
-                  ( runM
-                      . Time.runTime
-                      . Reader.runReader (WorkingDir workingDir)
-                      . Log.runLoggingWithTime (logConfig logSeverity)
-                      . runCleanCache
-                      . cleanImage masterDark masterBias loadTiff
-                  )
-                  lights
+--  runM $
+-- Reader.runReader (WorkingDir workingDir) $
+--   Time.runTime $
+--     Log.runLoggingWithTime (logConfig logSeverity) $
+--       runStarsLocatedCache $
+--         runMasterCache $ do
+--           Log.logInfo $ "Darks: " <> show (length darks)
+--           Log.logInfo $ "Biass: " <> show (length biass)
+--           Log.logInfo $ "Lights: " <> show (length lights)
+--           Log.logInfo $ "Working directory:  " <> workingDir
+--           (masterDark, masterBias) <-
+--             embed $
+--               runM
+--                 ( Reader.runReader (WorkingDir workingDir) $
+--                     Time.runTime $
+--                       Log.runLoggingWithTime (logConfig logSeverity) $
+--                         withTiming "MasterDark" $
+--                           runMasterCache $
+--                             calculateMaster "master_dark.tiff" darks
+--                 )
+--                 `Async.concurrently` runM
+--                   ( Reader.runReader (WorkingDir workingDir) $
+--                       Time.runTime $
+--                         Log.runLoggingWithTime (logConfig logSeverity) $
+--                           withTiming "MasterBias" $
+--                             runMasterCache $
+--                               calculateMaster "master_bias.tiff" biass
+--                   )
 
-            embed $
-              Async.mapConcurrently_
-                ( runM
-                    . Time.runTime
-                    . Reader.runReader (WorkingDir workingDir)
-                    . Log.runLoggingWithTime (logConfig logSeverity)
-                    . runStarsLocatedCache
-                    . locateStars
-                )
-                cleanImages
+--           cleanImages <-
+--             embed $
+--               Async.mapConcurrently
+--                 ( runM
+--                     . Time.runTime
+--                     . Reader.runReader (WorkingDir workingDir)
+--                     . Log.runLoggingWithTime (logConfig logSeverity)
+--                     . runCleanCache
+--                     . cleanImage masterDark masterBias loadTiff
+--                 )
+--                 lights
+
+--           embed $
+--             Async.mapConcurrently_
+--               ( runM
+--                   . Time.runTime
+--                   . Reader.runReader (WorkingDir workingDir)
+--                   . Log.runLoggingWithTime (logConfig logSeverity)
+--                   . runStarsLocatedCache
+--                   . locateStars
+--               )
+--               cleanImages
 
 ---------------------------------------------------------------
 -- Pipeline
